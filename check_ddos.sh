58a6dfaaf1a1b366c17c33eb18b1217562393058
davejonesbkk <davejonesbkk@gmail.com>
2018-01-26 13:22:44 +0700

    Update check_ddos.sh

--- check_ddos.sh
+++ check_ddos.sh
@@ -2,7 +2,5 @@
 
 #check for DDOS attacks
 
-netstat -an | egrep ":80|:443" |
-grep ESTABLISHED | awk '{print $5}' | grep -o -E "([0-9]{1,3}[\.]){3}[0,9]{1,3}"
-| sort -n | uniq -c | sort -nr 
+netstat -an | egrep ":80|:443" | grep ESTABLISHED | awk '{print $5}' | grep -o -E "([0-9]{1,3}[\.]){3}[0,9]{1,3}" | sort -n | uniq -c | sort -nr 
 
