a652fd8ec5de0e286e0d74cb4349cfbe08b4180e
David Jones <davejonesbkk@gmail.com>
2018-01-26 13:05:22 +0700

    First commit

---
+++ check_ddos.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#check for DDOS attacks
+
+netstat -an | egrep ":80|:443" |
+grep ESTABLISHED | awk '{print $5}' | grep -o -E "([0-9]{1,3}[\.]){3}[0,9]{1,3}"
+| sort -n | uniq -c | sort -nr 
+
